e00fef366a322fa25e51dd2b3088f2555c6e2c0d
madeline <qwertytrogi@gmail.com>
2026-02-06 09:04:00 -0800

    user program

--- gen.cpp
+++ gen.cpp
@@ -1,33 +1,42 @@
 #include <cstdint>
 #include <cstdio>
+#include <iostream>
+#include <string>
+#include <fstream>
 
 #include "gen.h"
 #include "moves.h"
+#include "SimpleIni.h"
 
-const Link initial_link = {
-    0.0f, -13015.9883, 0, 0.0f
-};
+#define TURN_MOVE_ID 0xFF
 
-uint64_t nodes = 0;
-
-const f32 min_z = -12977.2734-0.0025f; 
-const f32 max_z = -12977.2734+0.0025f;
-
-const f32 z_limit_min = -13015.9883;
-const f32 z_limit_max = -10000;
-
-const uint8_t max_depth = 7;
-const uint8_t max_cost = 10;
+uint64_t nodes;
+f32 min_z;
+f32 max_z;
+f32 z_limit_min;
+f32 z_limit_max;
+uint8_t max_depth;
+uint8_t max_cost;
+const char* output_filename;
+std::ofstream output_file;
 
 void recurse(Link link, uint8_t depth) {
     nodes++;
 
     if (link.z >= min_z && link.z <= max_z) {
-        printf("xfin: %f\tzfin: %f\t node: %llu\tdepth: %d\tcost: %d\tmoves: ", link.x, link.z, nodes, depth, link.cost);
+        std::cout << "pos_fin: "<<link.z<<"\tside_fin: "<<link.x<<"\tnodes: "<<nodes<<"\tdepth: "<<(int)depth<<"\tcost: "<<link.cost<<"\tmoves:";
         for (uint8_t i = 0; i < depth; i++) {
-            printf("%s(%f), ", moves[link.moves[i]].name, link.zs[i]);
+            std::cout << " " << moves[link.moves[i]].name << "(" << link.zs[i] << ")";
         }
-        printf("\n\n");
+
+        output_file << "pos_fin: "<<link.z<<"\tside_fin: "<<link.x<<"\tnodes: "<<nodes<<"\tdepth: "<<(int)depth<<"\tcost: "<<link.cost<<"\tmoves:";
+        for (uint8_t i = 0; i < depth; i++) {
+            output_file << " " << moves[link.moves[i]].name << "(" << link.zs[i] << ")";
+        }
+
+        std::cout << std::endl << std::endl;
+        output_file << std::endl << std::endl;
+
         return;
     }
 
@@ -46,9 +55,68 @@ void recurse(Link link, uint8_t depth) {
             }
         }
     }
+
+    if (link.moves[depth]) {
+        Link linkcopy = link;
+    }
 }
 
 int main() {
+	CSimpleIniA ini;
+	ini.SetUnicode();
+	SI_Error rc = ini.LoadFile("config.ini");
+
+	if (rc < 0) {
+        std::cout << "Could not find config.ini in the same folder as mtps.exe!";
+        system("pause");
+        return 1;
+    }
+
+    Link initial_link;
+
+    try {
+        const char* pv;
+        pv = ini.GetValue("config", "link_pos", "0.0");
+
+        initial_link = {
+            0.0f, std::stof(pv), 0, 0.0f
+        };
+
+        pv = ini.GetValue("config", "link_orientation", "1");
+        initial_link.orientation = std::stoi(pv);
+
+        uint64_t nodes = 0;
+
+        pv = ini.GetValue("config", "goal_min", "0.0");
+        min_z = std::stof(pv);
+
+        pv = ini.GetValue("config", "goal_max", "0.0");
+        min_z = std::stof(pv);
+
+        pv = ini.GetValue("config", "collision_limit_min", "0.0");
+        z_limit_min = std::stof(pv);
+
+        pv = ini.GetValue("config", "collision_limit_max", "0.0");
+        z_limit_max = std::stof(pv);
+
+        pv = ini.GetValue("config", "max_depth", "5");
+        max_depth = std::stoi(pv);
+
+        pv = ini.GetValue("config", "cost_max", "10");
+        max_cost = std::stoi(pv);
+
+        output_filename = ini.GetValue("config", "output_filename", "tp_setups.txt");
+        output_file = std::ofstream(output_filename);
+    }
+    catch (...) {
+        std::cout << "Failed to parse config.ini" << std::endl;
+        system("pause");
+        return 1;
+    }
+
+    std::cout << "Generating setups..." << std::endl;
+
     recurse(initial_link, 0);
+
     return 0;
 }
--- gen.h
+++ gen.h
@@ -1,13 +1,12 @@
 #pragma once
 
 #include <cstdint>
-#include "c_math.h"
 #include "types.h"
 
 struct Link {
     f32 x = 0;
     f32 z = 0;
-    s16 angle = 0;
+    int8_t orientation = 0;
     f32 speedF = 0;
     uint8_t moves[20];
     f32 zs[20];
@@ -26,10 +25,10 @@ struct Move {
 
     bool execute(Link& link, f32 z_limit_min, f32 z_limit_max) const {
         for (uint8_t i = 0; i < length; i++) {
-            link.x += x_trans[i];
-            link.z += z_trans[i];
-            link.x += x_speed[i];
-            link.z += z_speed[i];
+            link.x += x_trans[i] * link.orientation;
+            link.z += z_trans[i] * link.orientation;
+            link.x += x_speed[i] * link.orientation;
+            link.z += z_speed[i]*  link.orientation;
 
             if (link.z < z_limit_min || link.z > z_limit_max) {
                 return false;
